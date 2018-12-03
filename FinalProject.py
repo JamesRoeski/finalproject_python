fdbf9547ee14e84f7d81afd95bb1d26a6fc1cd79
James Roe (JCROE6803) <JCROE6803@ung.edu>
2018-12-03 17:29:29 -0500

    This is my first commit

---
+++ FinalProject.py
@@ -0,0 +1,28 @@
+"""
+
+final project - JCR
+jcroe6803@ung.edu
+
+"""
+
+import datetime
+import time
+
+# This program allows a user to pick a specific date and time then initializes a countdown
+# until the specified date and time has been reached
+
+
+
+month_chosen = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']
+
+# This function creates a date/time object chosen by the user and compares
+# this object to the current time and provides a countdown until that chosen time
+def count_down():
+
+
+    year = int(raw_input('What is the year of the date? '))
+    month = raw_input('What is the month of the date? ')
+    
+
+if __name__ == '__main__':  
+    count_down()   
